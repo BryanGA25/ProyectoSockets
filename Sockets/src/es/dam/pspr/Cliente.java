6489807cd0571daf5866972b417f682d41adccfb
Abel <abelvelardegarcia@gmail.com>
2020-03-06 19:40:25 +0100

    Menu en cliente

--- Sockets/src/es/dam/pspr/Cliente.java
+++ Sockets/src/es/dam/pspr/Cliente.java
@@ -1,4 +1,19 @@
 package es.dam.pspr;
 
 public class Cliente {
+
+    public Cliente(){
+        menu();
+    }
+
+    private void menu(){
+        System.out.println("******** MENU ********");
+
+        System.out.println("1) Leer el ultimo mensaje");
+        System.out.println("2) Escribir un nuevo mensaje");
+        System.out.println("3) Borrar un numero de mensajes");
+        System.out.println("4) Leer los ultimos 5 mensajes");
+        System.out.println("5) Borrar un mensaje especifico");
+        System.out.println("6) Salir");
+    }
 }
--- Sockets/src/es/dam/pspr/Servidor.java
+++ Sockets/src/es/dam/pspr/Servidor.java
@@ -6,20 +6,6 @@ public class Servidor {
         new Servidor();
     }
 
-    public Servidor(){
-        menu();
-    }
-
-    private void menu(){
-        System.out.println("******** MENU ********");
-
-        System.out.println("1) Leer el ultimo mensaje");
-        System.out.println("2) Escribir un nuevo mensaje");
-        System.out.println("3) Borrar un numero de mensajes");
-        System.out.println("4) Leer los ultimos 5 mensajes");
-        System.out.println("5) Borrar un mensaje especifico");
-        System.out.println("6) Salir");
-    }
 
 
 }
